ce7cff748360128f24b90a8c2d087e5ba1e8674c
Veyal <andrew.salim@binus.ac.id>
2018-11-04 18:27:30 +0700

    Updates

---
+++ precache-manifest.df5330b4e07fba2805d15ac7bb0749a5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef0647cd0f0c0ca003da3c42044d9127",
+    "url": "/HCI/static/media/guess.ef0647cd.png"
+  },
+  {
+    "revision": "1d317615073a9b6f0c4e",
+    "url": "/HCI/static/css/main.bbb4eff5.chunk.css"
+  },
+  {
+    "revision": "ee068c177163bc785216",
+    "url": "/HCI/static/js/1.ee068c17.chunk.js"
+  },
+  {
+    "revision": "f09cba9b26c79311fa19",
+    "url": "/HCI/static/js/runtime~main.f09cba9b.js"
+  },
+  {
+    "revision": "e5b61d534c09b6761ec83de23fc91b61",
+    "url": "/HCI/static/media/logo1.e5b61d53.png"
+  },
+  {
+    "revision": "127c17e1ee787c0c4d2e85180f871762",
+    "url": "/HCI/static/media/category.127c17e1.png"
+  },
+  {
+    "revision": "87183d4d57f5e75493294e9cad90a893",
+    "url": "/HCI/static/media/memorize.87183d4d.png"
+  },
+  {
+    "revision": "1d317615073a9b6f0c4e",
+    "url": "/HCI/static/js/main.1d317615.chunk.js"
+  },
+  {
+    "revision": "d0feb3779467c13b48e3bd3509101eec",
+    "url": "/HCI/static/media/things.d0feb377.jpg"
+  },
+  {
+    "revision": "83bc1bb8d18d5ba7b8a36ceb1ba7f4a6",
+    "url": "/HCI/static/media/sign.83bc1bb8.jpg"
+  },
+  {
+    "revision": "a1699b8c864ca639ee16ebb8bf63d4fd",
+    "url": "/HCI/static/media/computer.a1699b8c.gif"
+  },
+  {
+    "revision": "c0d3195cbb2065ffcbb721d601f0154d",
+    "url": "/HCI/static/media/kecap.c0d3195c.png"
+  },
+  {
+    "revision": "3a4c508de524df8f12b7df1a4938d469",
+    "url": "/HCI/static/media/hypnotist-circle.3a4c508d.png"
+  },
+  {
+    "revision": "b547506ab246edaa4f28208a0fbf1fa8",
+    "url": "/HCI/index.html"
+  }
+];
